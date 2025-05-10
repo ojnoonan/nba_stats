60bb95b8ec05de83476a967c0922ff72356a9f46
s3660457 <s3660457@student.rmit.edu.au>
2025-05-10 11:53:28 +1000

    more test fixes

--- Application/frontend/postcss.config.js
+++ Application/frontend/postcss.config.js
@@ -1,14 +1,9 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import postcssNesting from 'postcss-nesting'
-
-/** @type {import('postcss').Config} */
-export default {
+module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-nesting': {},
-    'tailwindcss/nesting': {},
+    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
-    autoprefixer: {},
-  },
+    autoprefixer: {}
+  }
 }
