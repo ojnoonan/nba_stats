4890374be70fb3922ff39082f0545b801fa234cf
s3660457 <s3660457@student.rmit.edu.au>
2025-05-10 11:20:29 +1000

    fixed issues

--- Application/frontend/postcss.config.js
+++ Application/frontend/postcss.config.js
@@ -1,9 +1,11 @@
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import postcssNesting from 'postcss-nesting'
 
 /** @type {import('postcss').Config} */
 export default {
   plugins: [
+    postcssNesting,
     tailwindcss,
     autoprefixer
   ]
