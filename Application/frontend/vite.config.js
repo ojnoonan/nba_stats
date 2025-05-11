93b063353301e483c9e96dff87625d15f85d1c90
s3660457 <s3660457@student.rmit.edu.au>
2025-05-11 15:32:01 +1000

    Refactor Dockerfile, postcss.config.cjs, vite.config.js, and docker-compose.yml to streamline build process, improve configuration, and remove unnecessary settings.

--- Application/frontend/vite.config.js
+++ Application/frontend/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '/src': path.resolve(__dirname, 'src')
-    }
-  },
   server: {
     port: 7779
   },
@@ -17,22 +11,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
-  },
-  css: {
-    postcss: './postcss.config.cjs',
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "./src/styles/variables.scss";'
-      }
-    }
+    assetsDir: 'assets'
   }
 })
