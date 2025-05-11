8577fff9772e75991f013914223945ea9295b3c4
s3660457 <s3660457@student.rmit.edu.au>
2025-05-11 18:15:53 +1000

    Update main.py to change FastAPI server port from 8000 to 7778

--- Application/backend/app/main.py
+++ Application/backend/app/main.py
@@ -174,4 +174,4 @@ async def reset_update_status(db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7778)
