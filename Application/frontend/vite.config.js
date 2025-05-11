af5f6abf98a9dcf7b5d96793e2d0b2fb7221148f
s3660457 <s3660457@student.rmit.edu.au>
2025-05-11 15:36:38 +1000

    Refactor Dockerfile, package.json, vite.config.js, and docker-compose.yml to enhance build process, improve dependency management, and streamline configurations.

--- Application/frontend/vite.config.js
+++ Application/frontend/vite.config.js
@@ -3,14 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 7779
-  },
-  preview: {
-    port: 7779
-  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: 'dist'
   }
 })
