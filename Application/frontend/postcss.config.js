a321bdee1228fdecbe885ff11ebbd1f16c7ffe24
s3660457 <s3660457@student.rmit.edu.au>
2025-05-11 15:27:16 +1000

    Refactor Dockerfile, package.json, and postcss.config.cjs to streamline build process, update dependency versions, and remove unnecessary configurations in docker-compose.yml.

--- Application/frontend/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-nesting': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
